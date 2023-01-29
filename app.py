ea7486bafe6192da61f043074e6b02afcd704c73
Daniel Lagos <lagos.d@gmail.com>
2023-01-29 01:16:59 -0800

    reverted back to old build.

--- app.py
+++ app.py
@@ -33,12 +33,12 @@ def predict():
     print(pred)
     return render_template('index.html', predict=str(pred))
 
-# if __name__ == '__main__':
-#     app.run
+if __name__ == '__main__':
+    app.run
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+#if __name__ == '__main__':
+#    port = int(os.environ.get('PORT', 5000))
+#    app.run(host='0.0.0.0', port=port)
