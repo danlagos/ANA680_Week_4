dfefb60984f2af7aaf0d067510b3a798ed663269
Daniel Lagos <lagos.d@gmail.com>
2023-01-29 02:15:22 -0800

    preping for docker.

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ def predict():
     print(pred)
     return render_template('index.html', predict=str(pred))
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     app.run
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
 
 #if __name__ == '__main__':
 #    port = int(os.environ.get('PORT', 5000))
